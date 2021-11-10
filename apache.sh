1329ee2898d049e96850d94a1afb1312e9dc2237
vikasvnath <41375990+vikasvnath@users.noreply.github.com>
2021-11-11 01:32:51 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for HOST in $(cat servers.txt )
 do
-ssh $HOST " hostname && echo '------------------------------------' && rpm -qa | egrep 'HEB_Tomcat9|HEB_tomcat-' |  cut -c 1-18 && rpm -qa | grep -i httpd | awk '/HEB_httpd-2.4.43/' && ps -C httpd >/dev/null && echo "Httpd is Running" || echo "Httpd is Not running" && echo -e '\n------------------------------------' " >> q1.txt
+ssh $HOST " hostname && echo '------------------------------------' && rpm -qa | egrep 'HEB_Tomcat9|HEB_tomcat-' |  cut -c 1-18 && rpm -qa | grep -i httpd | awk '/httpd-2./' && ps -C httpd >/dev/null && echo "Httpd is Running" || echo "Httpd is Not running" && echo -e '\n------------------------------------' " >> q1.txt
 done
